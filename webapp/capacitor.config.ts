d98562e88743218623aba2e03b80bc95a45182b0
aeoncl <c.lopez.dev@protonmail.com>
2026-01-29 16:55:21 +0100

    Revert "feat: add capacitorjs ios + android"
    This reverts commit b7b964361d1bd36b67f9c00076fd96e9cec7c459.

--- webapp/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.shlasouf.musicapp',
-  appName: 'my-music-app',
-  webDir: 'build'
-};
-
-export default config;
