f3233a5f26e9a3516457ee51d4d68cde3ee74d4e
aeoncl <c.lopez.dev@protonmail.com>
2026-02-03 23:58:36 +0100

    artist info

--- webapp/src/routes/(content)/artists/[artistId]/+page.svelte
+++ webapp/src/routes/(content)/artists/[artistId]/+page.svelte
@@ -39,9 +39,7 @@
             <h2>About</h2>
             <section class="about">
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nec nibh suscipit, dapibus ante auctor, mattis est. In tempor blandit sapien, sed venenatis libero elementum id. Nulla sit amet rhoncus massa. Pellentesque egestas neque in nibh sodales, vel scelerisque dolor tincidunt. Vivamus vehicula, neque vitae tempor lacinia, est justo posuere sem, a ultricies purus nisi sit amet mauris. Cras finibus sapien orci, sit amet molestie erat porta at. Nunc pharetra suscipit hendrerit.
-
-                    Aenean ligula felis, pellentesque vel dolor eu, maximus pellentesque tortor. Pellentesque euismod augue sit amet odio semper, ut luctus mi sagittis. Ut scelerisque dui sit amet dictum consectetur. Aenean accumsan felis in nulla faucibus egestas. Morbi mollis, diam at placerat suscipit, nisi nisi mollis sem, vitae mattis justo eros vel risus. Donec in nibh quis tellus pulvinar efficitur. Donec mauris est, bibendum sit amet mattis a, ultricies eu lectus. Aenean suscipit a metus quis gravida. Vestibulum vestibulum non velit non aliquam. Integer orci nisi, tempus et eleifend non, bibendum a nisi.
+                  {data.artistInfo.biography}
                 </p>
             </section>
 
--- webapp/src/routes/(content)/artists/[artistId]/+page.ts
+++ webapp/src/routes/(content)/artists/[artistId]/+page.ts
@@ -49,6 +49,39 @@ type Album = {
 	displayArtist: string;
 }
 
+type ArtistInfoResponse = {
+	"subsonic-response": {
+		status: string;
+		version: string;
+		type: string;
+		serverVersion: string;
+		openSubsonic: boolean;
+		artistInfo: ArtistInfo;
+	};
+}
+
+type ArtistInfo = {
+	biography: string;
+	musicBrainzId: string;
+	lastFmUrl: string;
+	smallImageUrl: string;
+	mediumImageUrl: string;
+	largeImageUrl: string;
+	similarArtist: SimilarArtist[];
+}
+
+type SimilarArtist = {
+	id: string;
+	name: string;
+	coverArt: string;
+	artistImageUrl: string;
+}
+
+function removeHtmlTags(input: string): string {
+	return input.replace(/<(\w+)[^>]*>[\s\S]*?<\/\1>|<[^>]*\/>/g, '');
+}
+
+
 export const load: PageLoad = async ({ fetch, params }) => {
 	//const artist = artists.find(artist => artist.id === Number(params.artistId));
 	const response = await fetch(
@@ -59,7 +92,38 @@ export const load: PageLoad = async ({ fetch, params }) => {
 
 	const artist = await response.json().then((res: ArtistsResponse) => res['subsonic-response'].artist);
 
-	console.log(artist);
+	const response2 = await fetch(
+		`${PUBLIC_NAVIDROME_URL}/getArtistInfo?u=${PUBLIC_NAVIDROME_USER}&t=${PUBLIC_NAVIDROME_TOKEN}&s=56228e&f=json&v=1.8.0&c=NavidromeUI&id=${params.artistId}`
+	);
+
+	const artistInfo = await response2
+		.json()
+		.then((res: ArtistInfoResponse) => res['subsonic-response'].artistInfo)
+		.then((artistInfo) => {
+			artistInfo.biography = removeHtmlTags(artistInfo.biography);
+			return artistInfo;
+		});
+
+
+/*
+	const response3 = await fetch(
+		`${PUBLIC_NAVIDROME_URL}/getTopSongs?u=${PUBLIC_NAVIDROME_USER}&t=${PUBLIC_NAVIDROME_TOKEN}&s=56228e&f=json&v=1.8.0&c=NavidromeUI&artist=${artist.name}`
+	);
+
+	console.log(response3);
+
+
+	const topSongs = await response3
+		.json()
+		.then((res: ArtistInfoResponse) => res['subsonic-response'].artistInfo)
+		.then((artistInfo) => {
+			artistInfo.biography = removeHtmlTags(artistInfo.biography);
+			return artistInfo;
+		});
+*/
+
+
+
 
 	if (!artist) {
 		redirect(500, '/error');
@@ -67,6 +131,7 @@ export const load: PageLoad = async ({ fetch, params }) => {
 
 	return {
 		artist: artist,
+		artistInfo: artistInfo,
 		getImagePath: (coverArt: string) => { return `${PUBLIC_NAVIDROME_URL}/getCoverArt?u=${PUBLIC_NAVIDROME_USER}&t=${PUBLIC_NAVIDROME_TOKEN}&s=56228e&f=json&v=1.8.0&c=NavidromeUI&id=${coverArt}&_=2025-12-12T13:08:37Z&size=300&square=true`; },
 		title: artist.name,
 		banner: {
